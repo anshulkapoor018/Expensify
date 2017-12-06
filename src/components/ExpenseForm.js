06f9c44b37eb3729e59f1e1b077418c7c414c6ba
Anshul Kapoor <anshulkapoor018@gmail.com>
2017-12-06 17:31:05 +0530

    Expense Form Complete

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,57 +1,104 @@
 import React from 'react';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
+// const date = new Date();
+const now = moment();
+console.log(now.format('MMM Do, YYYY'));
 
 export default class ExpenseForm extends React.Component {
   state = {
     description: '',
     note: '',
-    amount: ''
+    amount: '',
+    createdAt: moment(),
+    calendarFocused: false,
+    error: ''
   };
 
   onDescriptionChange = (e) => {
     const description = e.target.value;
-    this.setState(()=>({description}));
+    this.setState(() => ({ description }));
   };
 
   onNoteChange = (e) => {
     const note = e.target.value;
-    this.setState(()=>({note}));
+    this.setState(() => ({ note }));
   };
 
   onAmountChange = (e) => {
     const amount = e.target.value;
 
-    if(amount.match(/^\d*(\.\d{0,2})?$/)) {
-      this.setState(() => ({amount}));
+    if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
+      this.setState(() => ({ amount }));
     }
   };
 
-  render(){
-      return(
-          <div>
-            <form>
-                <input 
-                    type="text"
-                    placeholder="Description"
-                    autoFocus
-                    value = {this.state.description}
-                    onChange = {this.onDescriptionChange} 
-                /> <br/><br/>
-                <input 
-                    type="text"
-                    placeholder="Amount"
-                    value = {this.state.amount}
-                    onChange = {this.onAmountChange}
-                /> <br/><br/>
-                <textarea 
-                  placeholder="Add a note for your Expense(Optional)."
-                  value = {this.state.note}
-                  onChange = {this.onNoteChange}
-                >
-                  </textarea><br/><br/>
-                <button>Add Expense</button>
-            </form>
-          </div>
-      )
-  }   
-};
+  onDateChange = (createdAt) => {
+    if(createdAt){
+      this.setState(() => ({ createdAt }));      
+    }
+  };
 
+  onFocusChange = ({ focused }) => {
+    this.setState(() => ({ calendarFocused: focused }));
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    if(!this.state.description || !this.state.amount){
+      //Set error state to display
+      this.setState(() => ({error: 'Please fill in the required fields'}))
+    } else {
+      this.setState(() => ({error: ''}))
+      this.props.onSubmit({
+        description: this.state.description,
+        amount: parseFloat(this.state.amount, 10),
+        createdAt: this.state.createdAt.valueOf(),
+        note: this.state.note
+      });
+      console.log("Submitted")
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.error && <h4>{this.state.error}</h4>}
+        <form onSubmit={this.onSubmit}>
+          <input
+            type="text"
+            placeholder="*Description"
+            autoFocus
+            value={this.state.description}
+            onChange={this.onDescriptionChange}
+          /><br/><br/>
+          <input
+            type="text"
+            placeholder="*Amount"
+            value={this.state.amount}
+            onChange={this.onAmountChange}
+          /><br/><br/>
+          <SingleDatePicker
+            date={this.state.createdAt}
+            onDateChange={this.onDateChange}
+            focused={this.state.calendarFocused}
+            onFocusChange={this.onFocusChange}
+            numberOfMonths={1}
+            isOutsideRange={() => false}
+          /><br/><br/>
+          <textarea
+            placeholder="Add a note for your expense (optional)"
+            value={this.state.note}
+            onChange={this.onNoteChange}
+          >
+          </textarea><br/>
+          <h5>* marked fields are required.</h5>
+          <button>Add Expense</button>
+        </form>
+      </div>
+    )
+  }
+}
